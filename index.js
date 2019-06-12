e5685dc71283a97f0764a545e97b0ab9365382f4
ichitadon <ichita_0121@hotmail.com>
2019-06-12 16:20:34 +0900

    ポケモンの日本語名検索機能を追加

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#app',
     data : {
+      input_poledex_num: "pikachu",
       select_pokedex_num: "pikachu",
       display_pokedex_num: null,
       name: null,
@@ -11,7 +12,9 @@ var app = new Vue({
       json:null,
       front_default: null,
       back_default: null,
-      color: null
+      color: null,
+      pokemon_jp_dict: null,
+      find_result: null
     },
     methods: {
       getPokemon: function() {
@@ -41,10 +44,40 @@ var app = new Vue({
               resolve()
             })
         })
+      },
+      findBy: function (list, value, column) {
+        return list.filter(function (item) {
+            return (item[column] == value)
+        })
+      },
+      getPokemon_dict: function() {
+        new Promise((resolve, reject) => {
+          axios
+            .get('https://raw.githubusercontent.com/kotofurumiya/pokemon_data/master/data/pokemon_data.json')
+            .then(response => {
+              this.pokemon_jp_dict = response.data,
+              resolve()
+            })
+        })
+      },
+      getPokemonIdbyName: function() {
+        this.find_result = this.findBy(this.pokemon_jp_dict, this.select_pokedex_num, 'name')
+        if (this.find_result.length != 0) {
+          return this.find_result[0].no
+        } else {
+          return this.select_pokedex_num
+        }
+      },
+      showPokemonInfo: function() {
+        this.select_pokedex_num = this.input_poledex_num
+        this.select_pokedex_num = this.getPokemonIdbyName()
+        this.getPokemon()
+        this.getPokemon_species()
       }
     },
     mounted: function() {
       this.getPokemon()
       this.getPokemon_species()
+      this.getPokemon_dict()
     }
 });
